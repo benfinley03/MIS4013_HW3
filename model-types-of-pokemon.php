c0b0f18f3aa9a85942b20663f71cc1adb0418ee7
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 02:59:29 -0500

    Update model-types-of-pokemon.php

--- model-types-of-pokemon.php
+++ model-types-of-pokemon.php
@@ -7,7 +7,7 @@ function selectTypeOfPokemon() {
      		                            WHERE pt.Second_TypeID is NOT null and pt.PokemonID = p.PokemonID) as Type2
                                 FROM Type t JOIN PokeType_Relationship pt on t.TypeID = pt.TypeID 
 			                          JOIN Pokemon p on pt.PokemonID = p.PokemonID ");
-        $stmt->bind_param("", $p);
+        $stmt->bind_param("p", $PokemonName);
 	$stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
