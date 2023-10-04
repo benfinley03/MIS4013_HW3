93cf0b5f8295b72ca63325420ba12cb84c1eb345
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 04:36:26 -0500

    Update view-pokemon-moves.php

--- view-pokemon-moves.php
+++ view-pokemon-moves.php
@@ -1,17 +1,30 @@
 <h1>Pokemon Moves</h1>
-
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Pokemon ID</th>
+      <th>PokemonName</th>
+      <th>MoveName</th>
+      <th>MovePower</th>
+      <th>Move Accuracy</th>
+      </tr>
+    </thead>
+    <tbody>
 <?php
+while ($move = $moveOfPokemon->fetch_assoc()) {
+?> 
   <tr>
-    <td><?php echo $moves['PokemonName']; ?></td>
-    <td><?php echo $moves['MoveName']; ?></td>
-    <td><?php echo $moves['MovePower']; ?></td>
-    <td><?php echo $moves['MoveAccuracy']; ?></td>
+  <td><?php echo $move['PokemonID']; ?></td>
+  <td><?php echo $move['PokemonName']; ?></td>
+  <td><?php echo $move['MoveName']; ?></td>
+  <td><?php echo $move['MovePower']; ?></td>
+  <td><?php echo $move['MoveAccuracy']; ?></td>
+
   </tr>
 <?php  
 }
 ?> 
-</div>
-
     </tbody>
   </table>
 </div>
