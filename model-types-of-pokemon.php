88b14ba9ef700bb21f6d486ed6160663b57a1cca
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 02:38:37 -0500

    Create model-types-of-pokemon.php

---
+++ model-types-of-pokemon.php
@@ -0,0 +1,19 @@
+<?php
+function selectTypeOfPokemon() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.PokemonName, t.TypeName, 
+		                              (SELECT t.TypeName From Type t JOIN PokeType_Relationship pt on t.TypeID = pt.TypeID 
+     		                            WHERE pt.Second_TypeID is NOT null and pt.PokemonID = p.PokemonID) as Type2
+                                FROM Type t JOIN PokeType_Relationship pt on t.TypeID = pt.TypeID 
+			                          JOIN Pokemon p on pt.PokemonID = p.PokemonID ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
