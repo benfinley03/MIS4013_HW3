24b04262054baaf790fd72b9054669b811750779
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 03:57:26 -0500

    Update view-moves.php

--- view-moves.php
+++ view-moves.php
@@ -5,8 +5,7 @@
       <tr>
       <th>ID</th>
       <th>Name</th>
-      <th>Power</th>
-      <th>Accuracy</th>
+
       </tr>
     </thead>
     <tbody>
@@ -16,8 +15,7 @@ while ($move = $moves->fetch_assoc()) {
   <tr>
     <td><?php echo $move['MoveID']; ?></td>
     <td><?php echo $move['MoveName']; ?></td>
-    <td><?php echo $move['MovePower']; ?></td>
-    <td><?php echo $move['MoveAccuracy']; ?></td>
+
   </tr>
 <?php  
 }
