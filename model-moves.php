4a2a7b9357c5f450c2deffb984724fd7ca5fe2f2
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 01:54:50 -0500

    Update model-moves.php

--- model-moves.php
+++ model-moves.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectMoves() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM Moves;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
