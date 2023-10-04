a1ecfb1882946a30b20ab1d106f361576bfe94c1
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 01:08:08 -0500

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,1 +1,22 @@
-
+<section>
+<head>
+    <style>
+        #footer {
+            position: fixed;
+            padding: 5px 5px 0px 5px;
+            bottom: 0;
+            height: 25px;
+            background: grey;
+            right: 0;
+        }
+    </style>
+</head>
+    
+<footer id="footer">
+  <div id="footer-content"><h6> This is my footer for the homework.</h6></div>
+</footer>
+      </section>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
