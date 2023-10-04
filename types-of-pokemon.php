6c7378478f5a4b63e860ee0eb50a3de047c3926c
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 02:44:51 -0500

    Update types-of-pokemon.php

--- types-of-pokemon.php
+++ types-of-pokemon.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-types-of-pokemon.php");
 $pageTitle = "Types of Pokemon";
 include "view-header.php";
-$typesOfPokemon = selectTypeOfPokemon($_GET['id']);
+$typeOfPokemon = selectTypeOfPokemon($_GET['id']);
 include "view-types-of-pokemon.php";
 include "view-footer.php";
 ?>
