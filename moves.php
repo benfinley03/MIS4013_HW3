651c97772de27b752d4259b6e0f6deefe7d8573f
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 01:53:42 -0500

    Update moves.php

--- moves.php
+++ moves.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-moves.php");
+$pageTitle = "Moves";
+include "view-header.php";
+$moves = selectMoves();
+include "view-moves.php";
+include "view-footer.php";
+?>
