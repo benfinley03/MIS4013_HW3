589c81999fd5fec452cba8d4826ae7c2796b866b
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-04 01:57:56 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,1 +1,27 @@
 <h1>Moves</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Power</th>
+      <th>Accuracy</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($poke = $pokemon->fetch_assoc()) {
+?> 
+  <tr>
+    <td><?php echo $poke['MoveID']; ?></td>
+    <td><?php echo $poke['MoveName']; ?></td>
+    <td><?php echo $poke['MovePower']; ?></td>
+    <td><?php echo $poke['MoveAccuracy']; ?></td>
+  </tr>
+<?php  
+}
+?> 
+    </tbody>
+  </table>
+</div>
