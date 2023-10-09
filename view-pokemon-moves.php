95b87e4fd4ca4a284f9786252e476a8a6677bdee
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-09 14:25:15 -0500

    Update view-pokemon-moves.php

--- view-pokemon-moves.php
+++ view-pokemon-moves.php
@@ -6,8 +6,8 @@ while ($pokemove = $poke->fetch_assoc()) {
     <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <?php echo $move['PokemonName']; ?>
       </button>
+        <?php echo $move['PokemonName']; ?>
       </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div class="accordion-body">
