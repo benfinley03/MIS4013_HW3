62c909bb76a4e0c806e5bea3f88267a409c6ae7e
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-09 14:11:19 -0500

    Update pokemon-moves.php

--- pokemon-moves.php
+++ pokemon-moves.php
@@ -1,10 +1,11 @@
 <?php
 require_once("util-db.php");
 require_once("model-pokemon-moves.php");
+require_once("model-pokemon.php");
 $pageTitle = "Pokemon Moves";
 include "view-header.php";
 $pokemonMoves = selectPokemonMoves();
-$pokemove = selectPokemon();
+$poke = selectPokemon();
 include "view-pokemon-moves.php";
 include "view-footer.php";
 ?>
