e9e0abb097911ecb262b4d952ffc2fdd0c1568f1
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-09 14:53:32 -0500

    Update view-pokemon-moves.php

--- view-pokemon-moves.php
+++ view-pokemon-moves.php
@@ -12,7 +12,7 @@ while ($pokemove = $poke->fetch_assoc()) {
       <ul class="list-group">
 <?php
   $moves = selectPokemonMoves($move['MoveID']);
-while ($PokemonMoves = $moves->fetch_assoc()) {
+while ($pokemonMoves = $moves->fetch_assoc()) {
 ?> 
   <li class="list-group-item"><?php echo $move['MoveName']; ?></li>
   <li class="list-group-item"><?php echo $move['MovePower']; ?></li>
