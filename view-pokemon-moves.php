0307a6003e00c8c4fb8616856975d8807f1c487b
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-09 14:05:09 -0500

    Update view-pokemon-moves.php

--- view-pokemon-moves.php
+++ view-pokemon-moves.php
@@ -2,20 +2,30 @@
 <?php
 while ($move = $pokemon->fetch_assoc()) {
 ?>
-  
-}
+  <div <div class="accordion">
+    <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+      </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <?php echo $move['PokemonID']; ?>
+        <?php echo $move['PokemonName']; ?>
+      </div>
+    </div>
+  </div>
+    <ul class="list-group">
 <?php
 while ($move = $pokemonMoves->fetch_assoc()) {
 ?> 
-  <tr>
-  <td><?php echo $move['PokemonID']; ?></td>
-  <td><?php echo $move['PokemonName']; ?></td>
-  <td><?php echo $move['MoveName']; ?></td>
-  <td><?php echo $move['MovePower']; ?></td>
-  <td><?php echo $move['MoveAccuracy']; ?></td>
+  <li class="list-group-item"><?php echo $move['MoveName']; ?></li>
+  <li class="list-group-item"><?php echo $move['MovePower']; ?></li>
+  <li class="list-group-item"><?php echo $move['MoveAccuracy']; ?></li>
 
-  </tr>
 <?php  
+}
 }
 ?> 
     </tbody>
