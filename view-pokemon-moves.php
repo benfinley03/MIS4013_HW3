e63df710966462e2887bf25794a2acd5f57261b3
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-09 14:35:09 -0500

    Update view-pokemon-moves.php

--- view-pokemon-moves.php
+++ view-pokemon-moves.php
@@ -1,20 +1,14 @@
 <h1>Pokemon Moves</h1>
+<div class = "containers">
+  <div class = "card-group"
 <?php
 while ($pokemove = $poke->fetch_assoc()) {
 ?>
-  <div <div class="accordion">
-    <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-      </button>
-        <?php echo $move['PokemonName']; ?>
-      </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-      </div>
-    </div>
-
-    <ul class="list-group">
+  <div class = "card">
+    <div class = "card-body">
+      <h4 class = "card-title"><?php echo $move['PokemonName']; ?></h4>
+      <p class = "card-text">
+      <ul class="list-group">
 <?php
 while ($move = $pokemonMoves->fetch_assoc()) {
 ?> 
@@ -24,11 +18,7 @@ while ($move = $pokemonMoves->fetch_assoc()) {
 
 <?php  
 }
-?>
-</div>
-<?php
 }
 ?> 
-    </tbody>
-  </table>
+
 </div>
