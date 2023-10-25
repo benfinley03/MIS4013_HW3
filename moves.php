d49d6c6846ec5e022defc7575ef8bc533e860eb6
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 06:27:40 -0500

    Update moves.php

--- moves.php
+++ moves.php
@@ -3,6 +3,38 @@ require_once("util-db.php");
 require_once("model-moves.php");
 $pageTitle = "Moves";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertMove($_POST['mName'], $_POST['mPower'], $_POST['mAccuracy']))
+      {
+        echo '<div class="alert alert-success" role="alert">Move Added.</div>';
+      } else 
+      {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateMove($_POST['mName'], $_POST['mPower'], $_POST['mAccuracy'], $_POST['pID']))
+      {
+        echo '<div class="alert alert-success" role="alert">Move Edited.</div>';
+      } else 
+      {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deletePokemon($_POST['mID'])) 
+      {
+        echo '<div class="alert alert-success" role="alert">Move Deleted.</div>';
+      } else 
+      {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
 $moves = selectMoves();
 include "view-moves.php";
 include "view-footer.php";
