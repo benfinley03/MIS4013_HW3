74aa58c791fd48d478ba9a7fec86f7581e9e4866
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 05:25:24 -0500

    Update view-pokemon.php

--- view-pokemon.php
+++ view-pokemon.php
@@ -41,9 +41,6 @@ while ($poke = $pokemon->fetch_assoc()) {
 
     <td>
 <div class = "row">
-  <div class = "col">
-<h1>Pokemon</h1>
-  </div>
   <div class = "col-auto">
 <?php
 include "view-pokemon-editform.php";
