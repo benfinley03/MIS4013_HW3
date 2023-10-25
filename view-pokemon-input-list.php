d7016fcce07bb74a9cf0c9ae4f6f1512c65d1f8e
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 11:04:03 -0500

    Create view-pokemon-input-list.php

---
+++ view-pokemon-input-list.php
@@ -0,0 +1,10 @@
+<select class="form-select" id="pmPID" name="pmPID">
+<?php
+while ($pokemonItem = $PokemonList->fetch_assoc())
+{
+?>
+    <option value="<? php echo pokemonItem['PokemonID']; ?>"><? php echo pokemonItem['PokemonName']; ?></option>
+<?php
+}
+?>
+</select>
