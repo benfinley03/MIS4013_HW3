126c1732ebf31cf7adf048569f2fc18269c817b0
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 06:45:59 -0500

    Update view-moves-newform.php

--- view-moves-newform.php
+++ view-moves-newform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newTypeModal">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newMoveModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
       <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -7,18 +7,26 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newTypeModal" tabindex="-1" aria-labelledby="newTypeModalLabel" aria-hidden="true">
+<div class="modal fade" id="newMoveModal" tabindex="-1" aria-labelledby="newMoveModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newTypeModalLabel">New Type</h1>
+        <h1 class="modal-title fs-5" id="newMoveModalLabel">New Move</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
           <div class="mb-3">
-            <label for="tName" class="form-label">Type Name</label>
-            <input type="text" class="form-control" id="tName" name="tName">
+            <label for="mName" class="form-label">Move Name</label>
+            <input type="text" class="form-control" id="mName" name="mName">
+          </div>
+          <div class="mb-3">
+            <label for="mPower" class="form-label">Move Power</label>
+            <input type="integer" class="form-control" id="mPower" name="mPower">
+          </div>
+          <div class="mb-3">
+            <label for="mAccuracy" class="form-label">Move Accuracy</label>
+            <input type="integer" class="form-control" id="mAccuracy" name="mAccuracy">
           </div>
             <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
