7f45d136e1793b61490c93bc13d3ae1aa5498ff5
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 03:51:03 -0500

    Update pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -1,8 +1,18 @@
 <?php
 require_once("util-db.php");
 require_once("model-pokemon.php");
+
 $pageTitle = "Pokemon";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertPokemon($_POST['pName'], $_POST['pDesc']);
+      break;
+  }
+}
+
 $pokemon = selectPokemon();
 include "view-pokemon.php";
 include "view-footer.php";
