72245a180011ee8fcae6bc0ffc2f64a5f87b16a3
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 05:55:51 -0500

    Update model-types.php

--- model-types.php
+++ model-types.php
@@ -12,4 +12,48 @@ function selectType() {
         throw $e;
     }
 }
+
+function insertType($tName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Type` (`TypeName`) VALUES (?)");
+        $stmt->bind_param("s", $tName);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateType($pName, $pDesc, $pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Type` SET `TypeID`='[value-1]',`TypeName`='[value-2]' WHERE 1");
+        $stmt->bind_param("ssi", $pName, $pDesc, $pID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deletePokemon($pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Pokemon` WHERE `PokemonID` = ?");
+        $stmt->bind_param("i", $pID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
