a0414e9c82bf30ba93558b8e3dcde0b897005ea1
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 03:21:22 -0500

    Update model-pokemon.php

--- model-pokemon.php
+++ model-pokemon.php
@@ -13,3 +13,47 @@ function selectPokemon() {
     }
 }
 ?>
+
+function insertPokemon($pName, $pDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Pokemon` (`PokemonName`, `PokemonDesc`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $pName, $pDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updatePokemon($pName, $pDesc, $pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Pokemon` SET `PokemonName` = ?, `PokemonDesc` = ? WHERE `PokemonID` = ?");
+        $stmt->bind_param("ssI", $pName, $pDesc, $pID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deletePokemon($pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Pokemon` WHERE `PokemonID` = ?");
+        $stmt->bind_param("i", $pID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
