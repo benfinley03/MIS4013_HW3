58bd8b63112eb2943ee6d22edc6ad5418d392668
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 06:25:53 -0500

    Update model-moves.php

--- model-moves.php
+++ model-moves.php
@@ -12,4 +12,48 @@ function selectMoves() {
         throw $e;
     }
 }
+
+function insertMove($mName, $mPower, $mAccuracy) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Moves` (`MoveName`, `MovePower`, `MoveAccuracy`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sii", $mName, $mPower, $mAccuracy);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateMove($mName, $mPower, $mAccuracy, $mID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Moves` SET `MoveName` = ?, `MovePower` = ?, `MoveAccuracy`= ? WHERE `MoveID` = ?");
+        $stmt->bind_param("siii", $mName, $mPower, $mAccuracy, $mID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deletePokemon($mID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Moves` WHERE `MoveID` = ?");
+        $stmt->bind_param("i", $mID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
