bde9adf423d636191f5275c2ed2e1fabf0a3177a
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 09:16:39 -0500

    Update model-pokemon-moves.php

--- model-pokemon-moves.php
+++ model-pokemon-moves.php
@@ -32,4 +32,76 @@ From Moves m JOIN PokeMove_Relationship pm on m.MoveID = pm.MoveID
         throw $e;
     }
 }
+
+function selectPokemonMovesForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Select MoveID, MoveName From `Moves` Order by MoveName");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectPokemonForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Select PokemonID, PokemonName From `Pokemon` Order by PokemonName");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertMoveset($msPokemonKey, $msMoveKey, $msName, $msPower, $msAccuracy) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Moves` (`MoveID`, `MoveName`, `MovePower`, `MoveAccuracy`) VALUES (?, ?, ?, ?)", "INSERT INTO `Pokemon`);
+        $stmt->bind_param("isii", $mName, $mPower, $mAccuracy);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateMove($mName, $mPower, $mAccuracy, $mID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Moves` SET `MoveName` = ?, `MovePower` = ?, `MoveAccuracy`= ? WHERE `MoveID` = ?");
+        $stmt->bind_param("siii", $mName, $mPower, $mAccuracy, $mID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deletePokemon($mID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Moves` WHERE `MoveID` = ?");
+        $stmt->bind_param("i", $mID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
