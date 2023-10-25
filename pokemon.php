0dcf30aacbbd99fab1d702fd6bd5ca68d656a831
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 04:07:49 -0500

    Update pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -8,9 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertPokemon($_POST['pName'], $_POST['pDesc'])) {
+      if (insertPokemon($_POST['pName'], $_POST['pDesc']))
+      {
         echo '<div class="alert alert-success" role="alert">Pokemon Added.</div>'
-      } else {
+      } else 
+      {
         echo '<div class="alert alert-danger" role="alert">Error</div>'
       }
       break;
