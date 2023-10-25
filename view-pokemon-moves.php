379e4893ee8ef58e9d42ed79a269cdbf03fa6d5c
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 01:17:40 -0500

    Update view-pokemon-moves.php

--- view-pokemon-moves.php
+++ view-pokemon-moves.php
@@ -9,14 +9,20 @@ while ($pokemonMoves = $pokemove->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $pokeMove['PokemonName']; ?></h5>
     <p class="card-text">
+        <ul classs = "list-group">
 <?php
     $moves = selectMoves($pokemove['MoveID']);
     while ($pokemonMoves = $pokemove->fetch_assoc()) {
 ?>
+  <li class = "list-group-item"><?php echo $pokemove['PokemonName']; ?> - <?php echo $pokemove['MoveName']; ?> - <?php echo $pokemove['MoveName']; ?> - <?php echo $pokemove['MovePower']; ?> - <?php echo $pokemove['MoveAccuracy']; ?></li>
+  <li class = "list-group-item"><?php echo $pokemove['MoveName']; ?></li>
+  <li class = "list-group-item"><?php echo $pokemove['MovePower']; ?></li>
+  <li class = "list-group-item"><?php echo $pokemove['MoveAccuracy']; ?></li>
 <?php
     
     }
 ?>
+        </ul>
         </p>
 
     </div>
