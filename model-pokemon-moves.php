88547f2c911461079ab19a81d7b9d99bcc0dedd0
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-10-25 02:07:07 -0500

    Update model-pokemon-moves.php

--- model-pokemon-moves.php
+++ model-pokemon-moves.php
@@ -16,4 +16,22 @@ function selectMoveOfPokemon($mid) {
         throw $e;
     }
 }
+
+<?php
+function selectPokemonMoves() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Select p.PokemonID, p.PokemonName, m.MoveName, m.MovePower, m.MoveAccuracy
+From Moves m JOIN PokeMove_Relationship pm on m.MoveID = pm.MoveID
+				JOIN Pokemon p on pm.PokemonID = p.PokemonID");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
